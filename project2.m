75cc7e270df2aa7a4c473a3eef12bde8ca20cce7
saurav singh <sauravsingh2288@gmail.com>
2020-04-20 18:05:19 +0530

    Plotting Temperature

---
+++ project2.m
@@ -0,0 +1,125 @@
+function varargout = project2(varargin)
+% PROJECT2 MATLAB code for project2.fig
+%      PROJECT2, by itself, creates a new PROJECT2 or raises the existing
+%      singleton*.
+%
+%      H = PROJECT2 returns the handle to a new PROJECT2 or the handle to
+%      the existing singleton*.
+%
+%      PROJECT2('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in PROJECT2.M with the given input arguments.
+%
+%      PROJECT2('Property','Value',...) creates a new PROJECT2 or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before project2_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to project2_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help project2
+
+% Last Modified by GUIDE v2.5 07-Feb-2020 16:06:23
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @project2_OpeningFcn, ...
+                   'gui_OutputFcn',  @project2_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before project2 is made visible.
+function project2_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to project2 (see VARARGIN)
+
+% Choose default command line output for project2
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes project2 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = project2_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+    varargout{1} = handles.output;
+    clear all;
+    global a;
+    a = arduino;
+
+
+
+% --- Executes on button press in start.
+function start_Callback(hObject, eventdata, handles)
+% hObject    handle to start (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+    x = 0;
+    go = true;
+    global a;
+    while go
+
+    value = readVoltage(a,'A1');
+    temp = (value/1024)*50000;
+    disp(temp);
+    x = [x temp];
+    plot(handles.axes1,x);
+    grid on;
+    xlabel('Time(seconds)')
+    ylabel('Temperature(°C)');
+    title('Real-Time Temperature Graph');
+    drawnow
+    set(handles.edit1,'String',num2str(temp));
+    pause(1);
+    end
+
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
